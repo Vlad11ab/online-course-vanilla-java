c210bf5f9537ea379216d301ed8051ff17aa29ad
Vlad11ab <breazuvlad11@gmail.com>
2025-03-01 21:00:25 +0200

    added load functionality for Users using instanceof

--- untitled/src/app/User/service/UserService.java
+++ untitled/src/app/User/service/UserService.java
@@ -1,16 +1,59 @@
 package app.User.service;
 
+import app.User.model.Profesor;
+import app.User.model.Student;
+import app.User.model.User;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserService {
 
+    private List<User> users;
+
     public UserService() {
+        users = new ArrayList<>();
+        this.loadUsers();
 
-       
+        }
 
+        private loadUsers(){
+        String filename = "/Users/vlad11ab/Documents/mycode/incapsularea/untitled/src/app/User/data/Users.txt";
+
+        File file = new File(filename);
+
+        try(Scanner scanner = new Scanner(file)){
+
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String [] split = line.split(",");
+                switch(split[0]){
+                    case "STUDENT":
+                        this.users.add(new Student(line));
+                        break;
+                    case "PROFESOR":
+                        this.users.add(new Profesor(line));
+                        break;
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
 
 
         }
 
 
-    }
+        }
+
+
+
+
+
+
 
 }
